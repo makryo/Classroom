444e0e399bd2d7eb43146846db029b6f63763ef4
Mario Pacheco <roland350under@gmail.com>
2019-05-14 16:05:00 -0600

    links

--- actualizar.php
+++ actualizar.php
@@ -1,5 +1,9 @@
 <html>
 	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 		<title>Actualizar Datos</title>
 	</head>
 	<body background="images/marmol.jpg">
